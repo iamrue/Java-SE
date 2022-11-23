4de16f7c52c9a541c8e1acbc835e3a2ac5f74664
단비 <@RUE>
2022-11-23 13:15:20 +0900

    day02 basic

---
+++ src/day02/basic/MainClass04.java
@@ -0,0 +1,31 @@
+package day02.basic;
+/*
+ * [[기본 자료형]] 
+ * 
+ * 3. 정수형 
+ * - 선언 방법
+ * 	byte 변수명;
+ * 	short 변수명;
+ * 	int 변수명;
+ * 	long 변수명;
+ * 
+ * 
+ */
+public class MainClass04 {
+	public static void main(String[] args) {
+		//정수형 변수 선언과 동시에 값 대입하기
+		byte iByte =10;
+		short iShort=100;
+		int iInt = 1000;
+		long iLong = 10000;
+		// 저장되 값을 문자열로 출력해보기
+		System.out.println("byte 형:"+ iByte);
+		System.out.println("iShort 형:"+ iShort);
+		System.out.println("iInt 형:"+ iInt);
+		System.out.println("iLong 형:"+ iLong);
+		
+		
+	}
+	
+
+}
