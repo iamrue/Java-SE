fffa2d29db676d828f3436e7ead05ba64e83a4d7
단비 <@RUE>
2022-11-23 12:34:59 +0900

    day02 / 11월14일 수업

---
+++ src/day02/basic/MainClass02.java
@@ -0,0 +1,23 @@
+package day02.basic;
+/*
+ * [[ 기본 자료형 ]]
+ *  1. 논리형 (boolean)
+ *  
+ *  -선언방법
+ * 	boolean 변수명;
+ * - 가질 수 있는 범위 ; true, false 두가지 값 저장 가능
+ * 
+ * */
+
+public class MainClass02 {
+	public static void main(String[] args) {
+		// 논리형 지역변수 선언하기
+		boolean isWait;
+		// 선언한 변수에 값 대입하기
+		isWait = true;
+		System.out.print("논리형 변수에 저장된 값:"+isWait);
+	}
+	
+	 
+
+}
---
+++ src/day02/basic/MainClass03.java
@@ -0,0 +1,20 @@
+package day02.basic;
+
+public class MainClass03 {
+	public static void main(String[] args) {
+		char ch1 ='A';
+		System.out.println("char형 변수에 저장된 문자:" + ch1);
+		
+		char ch2 ='金';
+		char ch3 ='뿔';
+		
+		int num = ch1;//
+		System.out.println("대문자 A에 해당하는 숫자코드 :"+num);
+		
+		int num2 =110;
+		char ch4 = (char)num2; // int 를 char로 형변환
+		System.out.println(ch4);
+		
+	}
+
+}
